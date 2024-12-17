d8ef708be441a821b8ed7fd7c246761c4eb185a1
ghifarij <afga.ghifari@gmail.com>
2024-12-17 14:12:00 +0700

    Add get all events for pagination

--- src/controller/event.controller.ts
+++ src/controller/event.controller.ts
@@ -11,9 +11,31 @@ export class EventController {
           thumbnail: true,
           date: true,
           location: true,
+          venue: true,
         },
       });
       res.status(200).send({ events });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+  }
+
+  async getAllEvents(req: Request, res: Response) {
+    try {
+      const { page = 1, limit = 9 } = req.query;
+
+      const countEvents = await prisma.event.aggregate({
+        _count: { _all: true },
+      });
+      const totalPage = Math.ceil(countEvents._count._all / +limit);
+
+      const events = await prisma.event.findMany({
+        orderBy: { id: "asc" },
+        take: +limit,
+        skip: +limit * (+page - 1),
+      });
+      res.status(200).send({ totalPage, page: +page, events });
     } catch (err) {
       console.log(err);
       res.status(400).send(err);
