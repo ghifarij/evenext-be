75273228fe04de92f7637673c544817e91d961d4
kemalmusthafa <72489722+kemalmusthafa@users.noreply.github.com>
2024-12-19 15:51:50 +0700

    Add RefCode user (#8)
    * add auth user and login regist
    
    * add verify user
    
    * Add RefCode user

---
+++ src/controller/promotor.controller.ts
@@ -0,0 +1,143 @@
+import { Prisma } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import prisma from "../prisma";
+import { validate as isUuid } from "uuid";
+
+export class PromotorController {
+  async getPromotors(req: Request, res: Response) {
+    console.log(req.promotor);
+
+    try {
+      const { search, page = 1, limit = 5 } = req.query;
+      const filter: Prisma.PromotorWhereInput = {};
+
+      if (search) {
+        filter.OR = [
+          { username: { contains: search as string, mode: "insensitive" } },
+          { email: { contains: search as string, mode: "insensitive" } },
+        ];
+      }
+
+      const countPromotor = await prisma.promotor.count({ where: filter });
+      const total_page = Math.ceil(countPromotor / +limit);
+      const promotors = await prisma.promotor.findMany({
+        where: filter,
+        orderBy: { id: "asc" },
+        take: +limit,
+        skip: +limit * (+page - 1),
+      });
+
+      res.status(200).send({ total_page, page, promotors });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+  }
+
+  async getPromotorsId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { id } = req.params;
+
+      if (!isUuid(id)) {
+        res.status(400).json({ message: "Invalid UUID format" });
+        return;
+      }
+
+      const promotor = await prisma.promotor.findUnique({
+        where: { id },
+      });
+
+      if (!promotor) {
+        res.status(404).json({ message: "Promotor Not Found" });
+        return;
+      }
+
+      res.status(200).send({ promotor });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+  }
+
+  async createPromotor(req: Request, res: Response) {
+    try {
+      const { username, email, password } = req.body;
+
+      if (!username || !email || !password) {
+        res.status(400).json({ message: "Missing required fields" });
+        return;
+      }
+
+      const newPromotor = await prisma.promotor.create({
+        data: { username, email, password },
+      });
+
+      res.status(201).send({ message: "Promotor Created! ✅", promotor: newPromotor});
+    } catch (err: any) {
+      if (
+        err instanceof Prisma.PrismaClientKnownRequestError &&
+        err.code === "P2002"
+      ) {
+        res.status(400).json({
+          message: "Duplicate field value violates a unique constraint",
+        });
+        return;
+      }
+      console.log(err);
+      res.status(400).send(err);
+    }
+  }
+
+  async editPromotor(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const updatedPromotor = await prisma.promotor.update({
+        data: req.body,
+        where: { id },
+      });
+      res.status(200).send({ message: "Promotor Updated! ✅", promotor: updatedPromotor });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+  }
+
+  async deletePromotor(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      await prisma.promotor.delete({ where: { id } });
+      res.status(200).send("Promotor Deleted! ✅");
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+  }
+
+  async editAvatarPro(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { avatarUrl } = req.body;
+
+      if (!avatarUrl) {
+        res.status(400).json({ message: "Avatar URL is required" });
+        return;
+      }
+
+      const updatedPromotor = await prisma.promotor.update({
+        where: { id },
+        data: { avatar: avatarUrl },
+      });
+
+      res
+        .status(200)
+        .send({ message: "Avatar Updated! ✅", promotor: updatedPromotor });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+  }
+}
